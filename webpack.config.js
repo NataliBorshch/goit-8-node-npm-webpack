e8b95af309b3b9b969838f1474825895a3774993
NataliBorshch <67202688+NataliBorshch@users.noreply.github.com>
2020-11-30 20:21:09 +0200

    сборка

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+  devServer: {
+    port: 9999,
+    open: true,
+    stats: "errors-only",
+  },
+};
